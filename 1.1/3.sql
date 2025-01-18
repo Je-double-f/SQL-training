e7623e5f76d2e2817b347f4eeb0434db9a901e6d
Je-double-f <mainaccaunt052005@gmail.com>
2025-01-18 16:03:22 +0500

    task #3 is done

---
+++ 1.1/3.sql
@@ -0,0 +1,13 @@
+-- CREATE TABLE book1(
+--     book_id INTEGER PRIMARY KEY,
+--     title TEXT,
+--     author TEXT,
+--     price DECIMAL,
+--     amount INTEGER
+-- );
+-- INSERT INTO book1(book_id, title, author, price, amount)
+-- VALUES (1, 'Мастер и Маргарита', 'Булгаков М.А.', 670.99, 3),
+--        (2, 'Белая гвардия', 'Булгаков М.А.', 540.50,5),
+--        (3, 'Идиот', 'Достоевский Ф.М.',460.00, 10),
+--        (4, 'Братья Карамазовы', 'Достоевский Ф.М.', 799.01, 2);
+SELECT * FROM book1;
