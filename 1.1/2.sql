7d9878d2b95a502230e4106e1bf27b00d1e8e50f
Je-double-f <mainaccaunt052005@gmail.com>
2025-01-18 15:55:53 +0500

    task #2 is done

---
+++ 1.1/2.sql
@@ -0,0 +1,9 @@
+-- -- CREATE TABLE book(
+-- --     book_id INTEGER PRIMARY KEY,
+-- --     title TEXT,
+-- --     author TEXT,
+-- --     price TEXT,
+-- --     amount INTEGER
+-- -- );
+-- INSERT INTO book(book_id, title, author, price, amount)
+-- VALUES (1, 'Мастер и Маргарита', 'Булгаков М.А.', 670.99, 3);
