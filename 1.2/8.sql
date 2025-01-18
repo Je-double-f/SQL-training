1246dd41cc989155cdae6bd4340f9ccdc74641e6
Je-double-f <mainaccaunt052005@gmail.com>
2025-01-18 18:10:10 +0500

    task #8 for 1.2 is done

---
+++ 1.2/8.sql
@@ -0,0 +1,18 @@
+CREATE TABLE book(
+    book_id INTEGER PRIMARY KEY,
+    title TEXT,
+    author TEXT,
+    price DECIMAL,
+    amount INTEGER
+);
+
+INSERT INTO book(book_id, title, author, price, amount)
+VALUES (1, 'Мастер и Маргарита', 'Булгаков М.А.', 670.99, 3),
+       (2, 'Белая гвардия', 'Булгаков М.А.', 540.50,5),
+       (3, 'Идиот', 'Достоевский Ф.М.',460.00, 10),
+       (4, 'Братья Карамазовы', 'Достоевский Ф.М.', 799.01, 2),
+       (5, 'Стихотворения и поэмы', 'Есенин С.А.', 650.00, 15);
+
+SELECT title, author, price, amount
+FROM book
+WHERE (price < 500 OR price > 600) AND price * amount >= 5000;
