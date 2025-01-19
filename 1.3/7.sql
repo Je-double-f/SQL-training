27314ea93bc269a333e78023434804ac3c67f97a
Je-double-f <mainaccaunt052005@gmail.com>
2025-01-19 12:38:05 +0500

    task #7 for 1.3 is done

---
+++ 1.3/7.sql
@@ -0,0 +1,22 @@
+CREATE TABLE book(
+    book_id INTEGER PRIMARY KEY,
+    title TEXT,
+    author TEXT,
+    price DECIMAL,
+    amount INTEGER
+);
+
+INSERT INTO book(title, author, price, amount)
+VALUES ('Мастер и Маргарита', 'Булгаков М.А.', 670.99, 3),
+       ('Белая гвардия', 'Булгаков М.А.', 540.50,5),
+       ('Идиот', 'Достоевский Ф.М.',460.00, 10),
+       ('Братья Карамазовы', 'Достоевский Ф.М.', 799.01, 3),
+       ('Игрок', 'Достоевский Ф.М.', 480.50, 10),
+       ('Стихотворения и поэмы', 'Есенин С.А.', 650.00, 15);
+
+  SELECT author, SUM(price*amount) AS Стоимость
+    FROM book
+   WHERE title <> 'Белая гвардия' AND title <> 'Идиот'
+GROUP BY author
+  HAVING Стоимость > 5000
+ORDER BY Стоимость DESC
