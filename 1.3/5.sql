16398a4677fe2a5d716c979c5ece79b788ce70bb
Je-double-f <mainaccaunt052005@gmail.com>
2025-01-19 11:40:16 +0500

    task #5 for 1.3 is done

---
+++ 1.3/5.sql
@@ -0,0 +1,18 @@
+CREATE TABLE book(
+    book_id INTEGER PRIMARY KEY,
+    title TEXT,
+    author TEXT,
+    price DECIMAL,
+    amount INTEGER
+);
+
+INSERT INTO book(title, author, price, amount)
+VALUES ('Мастер и Маргарита', 'Булгаков М.А.', 670.99, 3),
+       ('Белая гвардия', 'Булгаков М.А.', 540.50,5),
+       ('Идиот', 'Достоевский Ф.М.',460.00, 10),
+       ('Братья Карамазовы', 'Достоевский Ф.М.', 799.01, 3),
+       ('Игрок', 'Достоевский Ф.М.', 480.50, 10),
+       ('Стихотворения и поэмы', 'Есенин С.А.', 650.00, 15);
+
+SELECT DISTINCT MIN(price) AS Минимальная_цена, MAX(price) AS Максимальная_цена, ROUND(AVG(price), 2) AS Средняя_цена
+  FROM book;
