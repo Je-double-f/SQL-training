3e42462c2c897757973838265aa6bd2dfdb6784e
Je-double-f <mainaccaunt052005@gmail.com>
2025-01-20 11:33:32 +0500

    task #3 is done

---
+++ 1.5/3.sql
@@ -0,0 +1,20 @@
+CREATE TABLE supply(
+       supply_id INTEGER PRIMARY KEY,
+       title TEXT,
+       author TEXT,
+       price DECIMAL(8,2),
+       amount INT
+);
+
+INSERT INTO supply(title, author, price, amount)
+VALUES  ('Лирика', 'Пастернак Б.Л.', 518.99, 2),
+        ('Чёрный человек', 'Есенин С.А.', 570.20, 6),
+        ('Белая гвардия', 'Булгаков М.А.', 540.50, 7),
+        ('Идиот', 'Достоевский Ф.М.', 360.80, 3);
+
+INSERT INTO book(title, author, price, amount)
+SELECT title, author, price, amount
+FROM supply
+WHERE author NOT IN ('Достоевский Ф.М.', 'Булгаков М.А.');
+
+SELECT * FROM book;
