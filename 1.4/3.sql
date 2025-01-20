99d10235ae13c214a67359dcb86e44223d3b13ca
Je-double-f <mainaccaunt052005@gmail.com>
2025-01-20 07:51:07 +0500

    task #3 for 1.4 is done

---
+++ 1.4/3.sql
@@ -0,0 +1,24 @@
+CREATE TABLE book(
+    book_id INTEGER PRIMARY KEY,
+    title TEXT,
+    author TEXT,
+    price DECIMAL,
+    amount INTEGER
+);
+
+INSERT INTO book(title, author, price, amount)
+VALUES ('Мастер и Маргарита', 'Булгаков М.А.', 670.99, 3),
+       ('Белая гвардия', 'Булгаков М.А.', 540.50,5),
+       ('Идиот', 'Достоевский Ф.М.',460.00, 10),
+       ('Братья Карамазовы', 'Достоевский Ф.М.', 799.01, 3),
+       ('Игрок', 'Достоевский Ф.М.', 480.50, 10),
+       ('Стихотворения и поэмы', 'Есенин С.А.', 650.00, 15);
+
+SELECT author, title, amount
+FROM book
+WHERE amount IN(
+    SELECT amount
+    FROM book
+    GROUP BY amount
+    HAVING COUNT(amount) = 1
+);
