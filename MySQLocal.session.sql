88904c41a1da75c5e8b5ec1c106f15cb5c37b4c7
Je-double-f <mainaccaunt052005@gmail.com>
2025-01-24 21:05:04 +0500

    task #8 for 2.2 is done

--- MySQLocal.session.sql
+++ MySQLocal.session.sql
@@ -1,21 +1,21 @@
-  SELECT title, name_author, name_genre, price, amount
-    FROM author INNER JOIN book  ON author.author_id = book.author_id
-                INNER JOIN genre ON genre.genre_id = book.genre_id
-GROUP BY title, name_author, name_genre, price, amount, genre.genre_id
-  HAVING genre.genre_id IN(
-        SELECT genre_amount.genre_id
-          FROM (
-            SELECT genre_id, SUM(amount) AS sum_amount
-              FROM book
-          GROUP BY genre_id
-          ) AS genre_amount
-        INNER JOIN (
-              SELECT genre_id, SUM(amount) AS sum_amount
-                FROM book
-            GROUP BY genre_id
-            ORDER BY sum_amount DESC
-             LIMIT 1
-        ) AS max_amount
-        ON genre_amount.sum_amount = max_amount.sum_amount
-       )
-ORDER BY title ASC;
+--   SELECT title AS Название, name_author AS Автор,
+--        (
+--         SELECT book.amount + supply.amount
+--           FROM supply INNER JOIN book ON book.title = supply.title AND book.price = supply.price
+--        ) AS Количество;
+--     FROM author INNER JOIN book USING(author_id)
+--                 INNER JOIN supply ON book.title = supply.title
+-- GROUP BY Название, Автор, Количество;
+CREATE TABLE supply (
+  supply_id INT PRIMARY KEY,
+  title VARCHAR(255),
+  author VARCHAR(255),
+  price DECIMAL(10, 2),
+  amount INT
+);
+
+INSERT INTO supply (supply_id, title, author, price, amount) 
+VALUES (1, 'Доктор Живаго', 'Пастернак Б.Л.', 618.99, 3),
+       (2, 'Черный человек', 'Есенин С.А.', 570.20, 6),
+       (3, 'Евгений Онегин', 'Пушкин А.С.', 440.80, 5),
+       (4, 'Идиот', 'Достоевский Ф.М.', 360.80, 3);
