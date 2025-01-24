8324da67cd1311a11c677ce31d3e7b45537531ac
Je-double-f <mainaccaunt052005@gmail.com>
2025-01-24 11:55:37 +0500

    task #7 for 2.2 is done

--- MySQLocal.session.sql
+++ MySQLocal.session.sql
@@ -1,3 +1,21 @@
-SELECT name_author, title, name_genre 
-  FROM genre INNER JOIN book ON genre.genre_id = book.genre_id 
-             INNER JOIN author ON author.author_id = book.author_id;
+  SELECT title, name_author, name_genre, price, amount
+    FROM author INNER JOIN book  ON author.author_id = book.author_id
+                INNER JOIN genre ON genre.genre_id = book.genre_id
+GROUP BY title, name_author, name_genre, price, amount, genre.genre_id
+  HAVING genre.genre_id IN(
+        SELECT genre_amount.genre_id
+          FROM (
+            SELECT genre_id, SUM(amount) AS sum_amount
+              FROM book
+          GROUP BY genre_id
+          ) AS genre_amount
+        INNER JOIN (
+              SELECT genre_id, SUM(amount) AS sum_amount
+                FROM book
+            GROUP BY genre_id
+            ORDER BY sum_amount DESC
+             LIMIT 1
+        ) AS max_amount
+        ON genre_amount.sum_amount = max_amount.sum_amount
+       )
+ORDER BY title ASC;
