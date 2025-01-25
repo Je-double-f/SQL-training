2b92f7cff329cacdd0c6528487dfe59d61b34e4f
Je-double-f <mainaccaunt052005@gmail.com>
2025-01-25 09:13:43 +0500

    nothing

--- MySQLocal.session.sql
+++ MySQLocal.session.sql
@@ -1,2 +1,8 @@
-SELECT * FROM genre
-WHERE genre_id IN(1,2);
+SELECT DISTINCT buy_id, title, price, buy_book.amount
+FROM 
+    book 
+    INNER JOIN buy_book USING(book_id)
+    INNER JOIN buy USING(buy_id)
+    INNER JOIN client USING(client_id)
+WHERE name_client = 'Баранов Павел'
+ORDER BY buy_id ASC, title ASC;
